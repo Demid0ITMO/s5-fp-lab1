74567ffa1d4a671c8bb15e18da25f4f23e5369cd
Demid0 <demid04ka@mail.ru>
2025-10-11 15:14:52 +0300

    Initial commit

---
+++ bin/main.ml
@@ -0,0 +1,5 @@
+(** Copyright 2025-2025, Lab1 Ryazanov Demid 367522 *)
+
+(** SPDX-License-Identifier: NONE *)
+
+let () = print_endline "Hello, World!"
---
+++ test/test_lab1.ml
@@ -0,0 +1,4 @@
+open OUnit2
+
+let tests = "factorial tests" >::: []
+let () = run_test_tt_main tests
